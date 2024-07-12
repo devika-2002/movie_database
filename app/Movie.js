1b6ca6b7684f9e223be70fbd59091ccd94e45ca2
Devika kumari <devika21@navgurukul.org>
2024-07-12 18:11:28 +0530

    prototype  done

---
+++ app/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./movie.css";
+function Movie() {
+  return (
+    <div className='movie-main-row'>
+        <div className='col1'>1</div>
+
+        <div className='col2'>
+            <div className='col-1'>1</div>
+            <div className='col-2'>2</div>
+            <div className='col-3'>3</div>
+        </div>
+  </div>
+  )
+}
+
+export default Movie;
