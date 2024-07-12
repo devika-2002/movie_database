4188b75c6f4da00239ece7a0b0179c64076e6177
Devika kumari <devika21@navgurukul.org>
2024-07-12 22:12:55 +0530

    fetch data from movie api log in a console

--- app/Header.js
+++ app/Header.js
@@ -1,18 +1,36 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 
 function Header() {
-  return (
-    <div className="container">
-        <h1>Movies Search </h1>
-        <div className="search-container">
-            <div className='search-box'>
-                <img className="img-search" src="Frame.png" alt="Search" />
-                <input type="text" placeholder='Search a movie'/>
-                <button className='Button' type='submit'>Search</button>
+    const [input, setInput] = useState('');
+
+    const handleSearch = () => {
+        const apiKey ='809f0cff';
+        fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    };
+
+    return (
+        <div className="container">
+            <h1>Movies Search</h1>
+            <div className="search-container">
+                <div className='search-box'>
+                    <img className="img-search" src="Frame.png" alt="Search" />
+                    <input 
+                        type="text" 
+                        placeholder='Search a movie'
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button className='Button' type='submit' onClick={handleSearch}>Search</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Header
+export default Header;
